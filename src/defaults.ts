10d387bd50ff1d0cc2aee131fc65e83230e0217d
Jon Cardona <hollywood@github.com>
2019-02-25 16:12:17 -0500

    Added config

---
+++ src/defaults.ts
@@ -0,0 +1,5 @@
+
+module.exports = {
+    FILE_NAME: '.github/add-members-to-team.yml',
+    ORG_WIDE_REPO_NAME: 'org-settings'
+  }
--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,34 @@ import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
   app.on('organization.member_added', async (context) => {
-    
+
+    const res = await context.github.repos.getContents({
+      owner: context.payload.repository.owner.login,
+      repo: 'org-settings',
+      path: '.github/add-member-to-team.yml'
+    })
+
+    const content = Buffer.from(res.data.content, 'base64').toString('utf8')
+
     const response = await context.github.query(`query ($login: String!, $team: String!) {
       organization (login: $login) {
         team (slug: $team) {
           id
         }
       }
-    }`, { login: "Albatoss", team: "Albatoss_Admin" })
+    }`, { login: "ORG_NAME", team: "DEFAULT_TEAM_NAME" })
 
     // The `Team` GraphQL type doesn't include the databaseId
     // so we can hack around it by decoding the node id.
     // See https://github.com/github/ecosystem-api/issues/1576
+
     const decoded = Buffer.from(response.organization.team.id, 'base64').toString()
     const split = decoded.split('Team')
     const id = parseInt(split[split.length - 1], 10)
 
     const teamParams = Object.assign({}, {
-      team_id: id,
-      username: context.payload.login
+      team_id: id || 0,
+      username: context.payload.login || ''
     } || {})
 
     await context.github.teams.addOrUpdateMembership(teamParams).catch((e) => console.log(e))
