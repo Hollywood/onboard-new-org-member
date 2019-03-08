a51e673eaf93cbcf035249e209d33326af3b6057
Jon Cardona <hollywood@github.com>
2019-03-08 14:37:05 -0500

    Added functionality to add an onboarding issue

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ export = (app: Application) => {
           id
         }
       }
-    }`, { login: config.orgName, team: config.defaultTeam })
+    }`, { login:config.orgName, team:config.defaultTeam })
 
     // The `Team` GraphQL type doesn't include the databaseId
     // so we can hack around it by decoding the node id.
@@ -30,12 +30,39 @@ export = (app: Application) => {
     const split = decoded.split('Team')
     const id = parseInt(split[split.length - 1], 10)
 
+   
     const teamParams = Object.assign({}, {
       team_id: id || 0,
       username: context.payload.membership.user.login || ''
     } || {})
 
     await context.github.teams.addOrUpdateMembership(teamParams).catch((e) => console.log(e))
+
+
+    const body = await context.github.query(`query ($owner: String!, $repo: String!) {
+      repository(owner: $owner, name: $repo) {
+        object(expression: "master:.github/templates/on-board-template.md") {
+          ... on Blob {
+            text
+          }
+        }
+      }
+    }`, { owner: config.orgName, repo:config.settingsRepo})
+
+    var issueBody = body.repository.object.text
+    issueBody = issueBody.replace("NEW USER", `@${context.payload.membership.user.login}`)
+    issueBody += (config.ccList) ? `\n\n<h6>/cc ${config.ccList}</h6>` : ''
+
+
+    const issueParams = {
+      owner: config.orgName,
+      repo: config.onboardRepo,
+      title:  `${context.payload.membership.user.login} On-boarding`,
+      body: issueBody
+    }
+
+    const createIssueParams = Object.assign({}, config.onboardRepo, issueParams || {})
+    context.github.issues.create(createIssueParams)
 
   })
 
